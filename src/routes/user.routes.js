53f1386c7e7f917059363634cba1f05bab8a3132
Sonu Gupta <143927223+Sonuu20@users.noreply.github.com>
2025-01-18 20:00:54 +0530

    updated user route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,7 +11,7 @@ import {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImg,
-} from "../controllers/user.controlller.js";
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
