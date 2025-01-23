65df39c1b3647661a20194b3b9b9dd87e8be2257
Sonu Gupta <143927223+Sonuu20@users.noreply.github.com>
2025-01-24 00:31:51 +0530

    created the protected like routes

---
+++ src/routes/like.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { getLikedVideos, toggleCommentLike, toggleTweetLike, toggleVideoLike } from "../controllers/like.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+router.use(verifyJWT); //applying verifyJWT middleware to all routes
+
+router.route("/toggle/v/:videoId").post(toggleVideoLike);
+router.route("/toggle/v/:commentId").post(toggleCommentLike);
+router.route("/toggle/v/:tweetId").post(toggleTweetLike);
+router.route("/videos").get(getLikedVideos);
+
+export default router;
