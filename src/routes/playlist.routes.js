10df5e8e5b3840069956b8bab103922b16befbf1
Sonu Gupta <143927223+Sonuu20@users.noreply.github.com>
2025-01-24 00:32:19 +0530

    created the playlist routes

---
+++ src/routes/playlist.routes.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  addVideoToPlaylist,
+  createPlaylist,
+  deletePlaylist,
+  getPlaylistById,
+  getUserPlaylist,
+  removeVideoFromPlaylist,
+  updatePlaylist,
+} from "../controllers/playlist.controller.js";
+
+const router = Router();
+router.use(verifyJWT); //Applying the JWT middleware to all routes
+
+router.route("/").post(createPlaylist);
+
+router
+  .route("/:playlistId")
+  .get(getPlaylistById)
+  .patch(updatePlaylist)
+  .delete(deletePlaylist);
+
+router.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist);
+router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
+
+router.route("/user/:userId").get(getUserPlaylist);
+
+export default router;
