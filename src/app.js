9becd0c1fa39b34467dcf39f09f9b5f1fd260e8c
Sonu Gupta <143927223+Sonuu20@users.noreply.github.com>
2025-02-14 00:16:12 +0530

    added the following routes in the app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
 const app = express();
 
@@ -32,10 +33,27 @@ app.use(express.static("public"));
 //for setting the cookie  in the user browser
 app.use(cookieParser());
 
+//HTTP request logger middleware for node.js
+app.use(morgan("dev"));
+
 //routes import
 import userRouter from "./routes/user.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 //routes delcration
 app.use("/api/v1/users", userRouter); //jaise hi users url hit hoga, control userRouter ke paas chala jayega
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 export { app };
