fc487b222c68bf5f49f0578df05d26883de73592
Sonu Gupta <143927223+Sonuu20@users.noreply.github.com>
2025-02-14 00:17:04 +0530

    updated the multer middleware

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,14 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {  //cb = callback
-    cb(null, "./public/temp")
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
-export const upload = multer({ storage })
+export const upload = multer({
+  storage,
+});
