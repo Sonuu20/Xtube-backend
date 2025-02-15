050154a05026ff1d1523ca4170ab4f315acfadcb
Sonu Gupta <143927223+Sonuu20@users.noreply.github.com>
2025-02-16 00:04:19 +0530

    updated the dashboard controller

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -3,11 +3,11 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   getChannelStats,
   getChannelVideos,
-} from "../controllers/dashboard.controller";
+} from "../controllers/dashboard.controller.js";
 
 const router = Router();
 
-router.route(verifyJWT); // Applying verifyJWT middleware to all routes in this file
+router.use(verifyJWT); // Applying verifyJWT middleware to all routes in this file
 
 router.route("/stats").get(getChannelStats);
 router.route("/videos").get(getChannelVideos);
