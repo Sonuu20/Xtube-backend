8d4095fe5f8b2c48381c5a7a97045ffe685c0ea0
Sonu Gupta <143927223+Sonuu20@users.noreply.github.com>
2025-03-01 20:54:17 +0530

    updated a minor bug

--- src/app.js
+++ src/app.js
@@ -45,8 +45,10 @@ import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 //routes delcration
+app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter); //jaise hi users url hit hoga, control userRouter ke paas chala jayega
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
